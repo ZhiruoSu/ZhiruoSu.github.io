fd85cc2c57fd17833e6047b66d1ca08160ab0378
ZhiruoSu <suzhiruo1994@outlook.com>
2018-12-03 16:17:51 +0800

    update
    debugging

--- final_project/scriptlib2.js
+++ final_project/scriptlib2.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-    $("button#hide_h2").click(function() {
-        $("h2").hide(500);
-    });
-
-    $("button#show_h2").click(function() {
-        $("h2").show(300);
-        $("h2").css("color","blue");
-        $("h2").html("You clicked me hard.");
-    });
-
     $("button#get_data").click(function() {
         var items = [];
         var i = 0;
